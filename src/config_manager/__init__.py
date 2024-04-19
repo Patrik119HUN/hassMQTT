16167632081e1778ff3355caf756acad3bb6aed9
Patrik119HUN <tukacs.patrik77@gmail.com>
2024-04-19 18:14:43 +0200

    config manager refactor

--- src/config_manager/__init__.py
+++ src/config_manager/__init__.py
@@ -1,3 +1,4 @@
+from io import TextIOWrapper
 import json
 from utils.singleton import SingletonMeta
 
@@ -6,14 +7,16 @@ class ConfigManager(metaclass=SingletonMeta):
     _data = None
 
     def __init__(self, path: str) -> None:
+        _file:TextIOWrapper = None
         try:
-            f = open(path, "r")
-            self._data = json.load(f)
-            f.close()
+            _file = open(path, "r")
+            self._data = json.load(_file)
         except FileNotFoundError:
             raise RuntimeError("Unable to find config.json")
         except ValueError:
             raise RuntimeError("Not valid JSON")
+        finally:
+            _file.close()
 
     def __getitem__(self, key: str):
         return self._data[key]
