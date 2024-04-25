f64aadc61972370a1be1c5be9d5c1f1919eeb494
Patrik119HUN <tukacs.patrik77@gmail.com>
2024-04-25 11:32:04 +0200

    basic light json encoder

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -1,12 +1,23 @@
 from shos.mqtt.mqtt_manager import MQTTManager
 from shos.modbus import get_modbus
 from shos.home_assistant import device
+from shos.home_assistant import light
+import json
+
+
+def SimpleEncoder(obj):
+    return obj.__dict__
 
 
 def main():
     Manager = MQTTManager()
     Modbus = get_modbus()
     Manager.subscribe("asd")
+    asztali = light.Light("Asztali lampa", light.Light.Type.RGBWW)
+    asztali.state = False
+    # asztali.set_color(255, 255, 123, 12, 222)
+    valami = json.dumps(asztali, cls=light.LightEncoder, indent=4)
+    # print(valami)
 
 
 if __name__ == "__main__":
--- shos/home_assistant/light.py
+++ shos/home_assistant/light.py
@@ -1,13 +1,15 @@
-from device import EntityInfo, Device, DeviceTypes
+from shos.home_assistant.device import EntityInfo, Device, DeviceTypes
 from dataclasses import dataclass
 from enum import Enum
+import json
+from shos.utils.clamp import clamp
 
 MAX_LIGHT_VALUE: int = 255
 
 
 class Light(EntityInfo):
     @dataclass
-    class Color:
+    class _Color:
         red: int
         green: int
         blue: int
@@ -25,9 +27,10 @@ class Light(EntityInfo):
             return str(self.value)
 
     _type: Type
-    _state: bool
-    _brightness: int
-    _color: Color = None
+    _state: bool = False
+    _brightness: int = 0
+    _color_temp: int = 0
+    _color: _Color = _Color(0, 0, 0, 0, 0)
 
     def __init__(self, name: str, light_type: Type):
         self.component = DeviceTypes.LIGHT
@@ -35,10 +38,11 @@ class Light(EntityInfo):
         self._type = light_type
         self.name = name
         self.unique_id = EntityInfo.generate_id()
-        self._color = Light.Color(0, 0, 0, 0, 0)
-        self._brightness = 0
-        self._state = False
         pass
+
+    @property
+    def type(self):
+        return self._type
 
     @property
     def brightness(self):
@@ -48,22 +52,16 @@ class Light(EntityInfo):
     def set_brightness(self, brightness: int) -> None:
         if self._type == Light.Type.BINARY:
             return
-        if brightness <= 0:
-            self._brightness = 0
-            self._state = False
-            return
-        if brightness > MAX_LIGHT_VALUE:
-            self._brightness = MAX_LIGHT_VALUE
-        else:
-            self._brightness = brightness
-        self._state = True
+        self._brightness = clamp(brightness, 0, MAX_LIGHT_VALUE)
+        self._state = True if self._brightness != 0 else False
+        return
 
     @property
     def state(self):
         return self._state
 
     @state.setter
-    def set_state(self, state: bool):
+    def state(self, state: bool):
         self._state = state
 
     @property
@@ -74,8 +72,25 @@ class Light(EntityInfo):
     def set_color(
         self, red: int, green: int, blue: int, white: int, warm_white: int
     ) -> None:
-        self._color.red = red
-        self._color.green = green
-        self._color.blue = blue
-        self._color.white = white
-        self._color.warm_white = warm_white
+        self._color.red = clamp(red, 0, MAX_LIGHT_VALUE)
+        self._color.green = clamp(green, 0, MAX_LIGHT_VALUE)
+        self._color.blue = clamp(blue, 0, MAX_LIGHT_VALUE)
+        self._color.white = clamp(white, 0, MAX_LIGHT_VALUE)
+        self._color.warm_white = clamp(warm_white, 0, MAX_LIGHT_VALUE)
+
+
+class LightEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Light):
+            return {
+                "brightness": obj.brightness,
+                "color_mode": str(obj.type),
+                "color": {
+                    "r": obj.color.red,
+                    "g": obj.color.green,
+                    "b": obj.color.blue,
+                    "w": obj.color.white,
+                },
+                "state": "ON" if obj.state is True else "OFF",
+            }
+        return json.JSONEncoder.default(self, obj)
