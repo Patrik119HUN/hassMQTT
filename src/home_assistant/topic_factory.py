140eb9a0edb7313b07266e5cf27fe43dea670f68
Patrik119HUN <tukacs.patrik77@gmail.com>
2024-06-02 11:57:30 +0200

    Topic factory can generate publishable topics

--- src/home_assistant/topic_factory.py
+++ src/home_assistant/topic_factory.py
@@ -5,13 +5,17 @@ import copy
 class HATopicFactory:
     __topic: Topic
 
-    def __init__(self, base_topic: str, device_type: str, device_id: str):
-        self.__topic = (
-            Topic(TopicType.PUBLISHER).add(base_topic).add(device_type).add(device_id)
-        )
+    def __init__(
+        self,
+        base_topic: str,
+        device_type: str,
+        device_id: str,
+        topic_type: TopicType = TopicType.SUBSCRIBER,
+    ):
+        self.__topic = Topic(topic_type).add(base_topic).add(device_type).add(device_id)
 
-    def create(self, *args) -> str:
+    def create(self, *args) -> Topic:
         new_topic = copy.deepcopy(self.__topic)
         for arg in args:
             new_topic.add(arg)
-        return new_topic.build()
+        return new_topic
