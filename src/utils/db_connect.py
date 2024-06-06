e2b5d994b5c8b5fe2ef80911323f3946dc2492b1
Patrik119HUN <tukacs.patrik77@gmail.com>
2024-06-06 21:31:22 +0200

    Database connect gives dictionary

--- src/utils/db_connect.py
+++ src/utils/db_connect.py
@@ -5,4 +5,7 @@ import sqlite3
 @contextmanager
 def connect(db_path: str):
     with sqlite3.connect(db_path) as conn:
+        conn.row_factory = lambda c, r: dict(
+            [(col[0], r[idx]) for idx, col in enumerate(c.description)]
+        )
         yield conn.cursor()
