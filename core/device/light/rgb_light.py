64da002ed74e74ef45b568b3ab2bdf51e9686055
Patrik119HUN <tukacs.patrik77@gmail.com>
2024-07-17 12:23:17 +0200

    default values set

--- core/device/light/rgb_light.py
+++ core/device/light/rgb_light.py
@@ -9,9 +9,11 @@ MAX_LIGHT_VALUE: int = 255
 
 class RGBLight(BrightnessLight):
     color_mode: str = "rgb"
-    __red: int = 0
-    __green: int = 0
-    __blue: int = 0
+    __red: int = 255
+    __green: int = 255
+    __blue: int = 255
+    __brightness: float = 1
+    __state: bool = False
 
     def __init__(
         self,
@@ -31,6 +33,28 @@ class RGBLight(BrightnessLight):
         )
 
     @property
+    def brightness(self) -> int:
+        return self.__brightness
+
+    @brightness.setter
+    def brightness(self, brightness: int) -> None:
+        self.__brightness = clamp(brightness, 0, MAX_LIGHT_VALUE) / 255
+        self.state = True if brightness != 0 else False
+
+    @property
+    def state(self):
+        return self.__state
+
+    @state.setter
+    def state(self, state: bool) -> None:
+        if state:
+            self.color = (self.__red, self.__green, self.__blue)
+        else:
+            for x in range(3):
+                self.driver.send_data(x, 0)
+        self.__state = state
+
+    @property
     def red(self) -> int:
         return self.__red
 
@@ -45,17 +69,17 @@ class RGBLight(BrightnessLight):
     @red.setter
     def red(self, value: int) -> None:
         self.__red = clamp(value, 0, MAX_LIGHT_VALUE)
-        self.driver.send_data(0, self.__red)
+        self.driver.send_data(0, int(self.__red*self.__brightness))
 
     @green.setter
     def green(self, value: int) -> None:
         self.__green = clamp(value, 0, MAX_LIGHT_VALUE)
-        self.driver.send_data(1, self.__green)
+        self.driver.send_data(1, int(self.__green*self.__brightness))
 
     @blue.setter
     def blue(self, value: int) -> None:
         self.__blue = clamp(value, 0, MAX_LIGHT_VALUE)
-        self.driver.send_data(2, self.__blue)
+        self.driver.send_data(2, int(self.__blue*self.__brightness))
 
     @property
     def color(self) -> tuple[int, int, int]:
